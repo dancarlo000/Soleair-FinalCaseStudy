b4fa34cde3460f7aac7b8e1ef994de6c092aad64
Ruka-300ms <andreipellana1014.com>
2025-12-09 20:27:56 +0800

    completed backend with migrations, seeding, and controllers

--- backend/src/app/Models/User.php
+++ backend/src/app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
         'address',       // For shipping/billing
         'phone',  // For contact
         'is_admin',      // For role management
-        'is_blocked',    
+        'is_blocked',    // <-- ADDED: For blocking/unblocking users
     ];
 
     /**
@@ -49,7 +49,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'is_admin' => 'boolean',
-            'is_blocked' => 'boolean', 
+            'is_blocked' => 'boolean', // <-- ADDED: Cast block status to boolean
         ];
     }
 
